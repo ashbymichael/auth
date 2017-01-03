3da105be3609fedd2cd397f437318abfa1066af1
Michael Ashby <ashby.michael@gmail.com>
2017-01-03 15:57:43 -0800

    fix password validation issue

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,14 @@
 class User < ActiveRecord::Base
   has_many :things
   validates :username, uniqueness: true
+  has_secure_password
 
-  def password
-    @password ||= BCrypt::Password.new(password_hash)
-  end
-
-  def password=(new_password)
-    @password = BCrypt::Password.create(new_password)
-    self.password_hash = @password
-  end
+  # def password
+  #   @password ||= BCrypt::Password.new(password_hash)
+  # end
+  #
+  # def password=(new_password)
+  #   @password = BCrypt::Password.create(new_password)
+  #   self.password_hash = @password
+  # end
 end
--- db/migrate/20161205111503_create_users.rb
+++ db/migrate/20161205111503_create_users.rb
@@ -2,7 +2,7 @@ class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
       t.string :username
-      t.string :password_hash
+      t.string :password_digest
     end
   end
 end
